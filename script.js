5fe150dd64911598e66ef3ee18bcf0d46eaec491
khushal <khushal2004a@gmail.com>
2024-08-17 00:34:02 +0530

    made the obstacles moving

--- script.js
+++ script.js
@@ -1,140 +1,101 @@
-// let canvas = document.getElementById('canvas');
-// let ctx = canvas.getContext("2d");
+let lowerDistX = 100;
+let upperDistX = 150;
+const obstSpacing = 250;
 
-// const width = canvas.width;
-// const height = canvas.height;
+function init() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext("2d");
+    const width = canvas.width;
+    const height = canvas.height;
 
-// let keys = [38];
+    const backgroundImg = new Image();
+    const groundImg = new Image();
+    const lowerObst = new Image();
+    const upperObst = new Image();
 
-// let backgroundImg = new Image();
-// backgroundImg.src = "public/background.png";
-
-// backgroundImg.onload = function() {
-//     ctx.drawImage(backgroundImg, 0, 0, width, height);
-// }
-
-// let birdImg = new Image();
-// birdImg.src = "public/bird.png";
-
-// birdImg.onload = function() {
-//     backgroundImg.drawImage(birdImg, 15, 300 - 75, 75, 75);
-// }
-
-// let groundImg = new Image();
-// groundImg.src = "public/ground.png";
-
-// groundImg.onload = function() {
-//     backgroundImg.drawImage(groundImg, 0, height - 100, width, 100);
-// }
-
-// let upperObst1 = new Image();
-// upperObst1.src = "public/upper.png";
-
-// upperObst1.onload = function() {
-//     backgroundImg.drawImage(upperObst1, 100, 0, 75, 250);
-// }
-
-// let upperObst2 = new Image();
-// birdImg.src = "public/upper.png";
-
-// upperObst2.onload = function() {
-//     ctx.drawImage(upperObst2, 15, 300 - 75, 75, 75);
-// }
-
-// let upperObst3 = new Image();
-// birdImg.src = "public/upper.png";
-
-// upperObst3.onload = function() {
-//     ctx.drawImage(upperObst3, 15, 300 - 75, 75, 75);
-// }
-
-// let lowerObst1 = new Image();
-// lowerObst1.src = "public/lower.png";
-
-// lowerObst1.onload = function() {
-//     backgroundImg.drawImage(lowerObst1, 70, height - 100 - 200, 75, 200);
-// }
-
-// let lowerObst2 = new Image();
-// birdImg.src = "public/lower.png";
-
-// lowerObst2.onload = function() {
-//     ctx.drawImage(lowerObst2, 15, 300 - 75, 75, 75);
-// }
-
-// let lowerObst3 = new Image();
-// birdImg.src = "public/lower.png";
-
-// lowerObst3.onload = function() {
-//     ctx.drawImage(lowerObst3, 15, 300 - 75, 75, 75);
-// }
-
-
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext("2d");
-
-const width = canvas.width;
-const height = canvas.height;
-
-let keys = [38];
-
-let backgroundImg = new Image();
-backgroundImg.src = "public/background.png";
-
-backgroundImg.onload = function() {
-    ctx.drawImage(backgroundImg, 0, 0, width, height);
-    drawBird();
-    drawGround();
-    drawUpperObstacle();
-    drawLowerObstacle();
-}
-
-function drawBird() {
-    let birdImg = new Image();
-    birdImg.src = "public/bird.png";
-
-    birdImg.onload = function() {
-        ctx.drawImage(birdImg, 15, 300 - 75, 75, 75);
-    }
-}
-
-function drawGround() {
-    let groundImg = new Image();
+    backgroundImg.src = "public/background.png";
     groundImg.src = "public/ground.png";
+    lowerObst.src = "public/lower.png";
+    upperObst.src = "public/upper.png"
 
-    groundImg.onload = function() {
+    let imagesLoaded = 0;
+    const totalImages = 4;
+
+    function imageLoaded() {
+        imagesLoaded++;
+        if (imagesLoaded === totalImages) {
+            startGame(ctx, width, height, backgroundImg, groundImg, lowerObst, upperObst);
+        }    
+    }
+
+    backgroundImg.onload = imageLoaded;
+    groundImg.onload = imageLoaded;
+    lowerObst.onload = imageLoaded;
+    upperObst.onload = imageLoaded;
+}
+
+function startGame(ctx, width, height, backgroundImg, groundImg, lowerObst, upperObst) {
+    const lowerObstacles = [
+        { x: lowerDistX + obstSpacing * 4, y: Math.floor(Math.random() * 181 + 100) },
+        { x: lowerDistX + obstSpacing * 3, y: Math.floor(Math.random() * 181 + 100)},
+        { x: lowerDistX + obstSpacing * 2, y: Math.floor(Math.random() * 181 + 100) },
+        { x: lowerDistX + obstSpacing, y: Math.floor(Math.random() * 181 + 100) },
+        { x: lowerDistX, y: Math.floor(Math.random() * 181 + 100) }
+    ]
+
+    const upperObstacles = [
+        { x: upperDistX + obstSpacing * 4, y: Math.floor(Math.random() * 181 + 100) },
+        { x: upperDistX + obstSpacing * 3, y: Math.floor(Math.random() * 181 + 100)},
+        { x: upperDistX + obstSpacing * 2, y: Math.floor(Math.random() * 181 + 100) },
+        { x: upperDistX + obstSpacing, y: Math.floor(Math.random() * 181 + 100) },
+        { x: upperDistX, y: Math.floor(Math.random() * 181 + 100) }
+    ]
+
+    function drawGround() {
         ctx.drawImage(groundImg, 0, height - 100, width, 100);
     }
+
+    function drawLowerObstacle() {
+        lowerObstacles.forEach(obstacle => {
+            ctx.drawImage(lowerObst, obstacle.x, height - 100 - obstacle.y, 75, obstacle.y);
+        });
+    }
+
+    function drawUpperObstacle() {
+        upperObstacles.forEach(obstacle => {
+            ctx.drawImage(upperObst, obstacle.x, 0, 75, obstacle.y);
+        });
+    }
+
+    function loop() {
+        update();
+        ctx.clearRect(0, 0, width, height);
+        ctx.drawImage(backgroundImg, 0, 0, width, height);
+        drawGround();
+        drawLowerObstacle();
+        drawUpperObstacle();
+        window.requestAnimationFrame(loop);
+    }
+
+    function update() {
+        lowerObstacles.forEach(obstacle => {
+            obstacle.x -= 2;
+
+            if (obstacle.x < -75) {
+                obstacle.x = 150 + obstSpacing * (lowerObstacles.length - 1);
+            }
+        });
+
+        upperObstacles.forEach(obstacle => {
+            obstacle.x -= 2;
+
+            if (obstacle.x < -75) {
+                obstacle.x = 150 + obstSpacing * (lowerObstacles.length - 1);
+            }
+        });
+    }
+
+    loop();
 }
 
-function drawUpperObstacle() {
-    let upperObst1 = new Image();
-    upperObst1.src = "public/upper.png";
-
-    upperObst1.onload = function() {
-        ctx.drawImage(upperObst1, 100, 0, 75, 250);
-    }
-}
-
-function drawLowerObstacle() {
-    let lowerObst1 = new Image();
-    lowerObst1.src = "public/lower.png";
-
-    lowerObst1.onload = function() {
-        ctx.drawImage(lowerObst1, 70, height - 100 - 200, 75, 200);
-    }
-
-    let lowerObst2 = new Image();
-    lowerObst2.src = "public/lower.png";
-
-    lowerObst2.onload = function() {
-        ctx.drawImage(lowerObst2, 500, height - 100 - 200, 75, 200);
-    }
-
-    let lowerObst3 = new Image();
-    lowerObst3.src = "public/lower.png";
-
-    lowerObst3.onload = function() {
-        ctx.drawImage(lowerObst3, 280, height - 100 - 200, 75, 200);
-    }
-}
+window.onload = init;
